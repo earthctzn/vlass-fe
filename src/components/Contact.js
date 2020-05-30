177a20a6f146b69e73085bdcef5d8facd215fb19
earthctzn <earthctzn1@gmail.com>
2020-05-30 16:01:25 -0400

    Adds black bar div to contact per the template provided.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,8 +8,8 @@ export default function Contact({ title, content, id, loading }) {
       <div className="section-content" id={id}>
         {loading}
         <h1>{title}</h1>
-        <hr></hr>
-        <h2>{content}</h2>
+        <div id="contact-bar"></div>
+        <h2 id="contact-h2">{content}</h2>
       </div>
     </div>
   );
