eb45e0e166519c20560f066df03287365cf17e58
earthctzn <earthctzn1@gmail.com>
2020-06-02 23:15:46 -0400

    Adds updateAbout patch request to about actions.

--- src/actions/aboutActions.js
+++ src/actions/aboutActions.js
@@ -1,4 +1,4 @@
-const setAboutTitle = ( abtData ) => {
+const setAbout = ( abtData ) => {
     return { type: "SET_ABOUT", payload: abtData }
 }
 const loadingAbout = () => {
@@ -15,9 +15,42 @@ export const fetchAbout = () => {
                     throw response
                 }
                 const abtData = await response.json()
-                dispatch(setAboutTitle(abtData))
+                dispatch(setAbout(abtData))
         }catch(data){
                 console.log(data)
         }
     }
 }
+
+export const updateAbout = (csrf_token, about) => {
+    return async function (dispatch) {
+        try{
+            const formData = { about: {
+                id: about.id,
+                title: about.title,
+                content: about.content
+            }};
+            const response = await fetch(`http://localhost:3000//api/v1/about/${about.id}`,{
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrf_token
+                },
+                body: JSON.stringify(formData),
+                credentials: 'include'
+            })
+            if (!response.ok) {
+                throw response
+            }
+            const abtData = await response.json()
+            if (abtData.errors) {
+                console.log(abtData.errors)
+            }else{
+                dispatch(setAbout(abtData))
+            }
+
+        } catch(data) {
+            console.log(data)
+        };
+    }
+};
