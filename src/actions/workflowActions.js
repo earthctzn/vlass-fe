0c93cc4f59aaa95dcccd6fdd56d8924a55813782
earthctzn <earthctzn1@gmail.com>
2020-06-03 12:25:26 -0400

    Adds post request to workflow actions.

--- src/actions/workflowActions.js
+++ src/actions/workflowActions.js
@@ -24,3 +24,35 @@ export const fetchWorkflow = () => {
         }
     }
 }
+
+export const updateWorkflow = (csrf_token, title, content, id) => {
+    return async function (dispatch) {
+        try{
+            dispatch(loadingWorkflow())
+            const formData = { workflow: {
+                id: id,
+                title: title,
+                content: content
+            }};
+            console.log("this is the form data", formData)
+            const response = await fetch(`http://localhost:3000/api/v1/workflow/${id}`,{
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrf_token
+                },
+                body: JSON.stringify(formData),
+                credentials: 'include'
+            })
+            if (!response.ok) {
+                throw response
+            }
+            const workflowData = await response.json()
+            dispatch(setWorkflow(workflowData))
+            
+
+        } catch(data) {
+            console.log(data)
+        };
+    }
+};
