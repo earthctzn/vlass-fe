502503e513218e173b24bfcb2cf78ec180d75806
earthctzn <earthctzn1@gmail.com>
2020-06-07 00:08:37 -0400

    Adds react spring and hamburger button image.

--- src/Nav.js
+++ src/Nav.js
@@ -2,8 +2,15 @@ import React, { Component } from "react"
 import { Link } from "react-scroll"
 import { connect } from 'react-redux'
 import { logOutUser } from './actions/userActions'
+import { Spring, animated } from 'react-spring/renderprops'
+import  hamburger  from './media/hamburger.png'
 
 class NavBar extends Component {
+    state={
+        toggle: true
+    }
+
+    onToggle = () => this.setState(state => ({ toggle: !state.toggle }))
 
     handleClick = () => {
         this.props.logOutUser( this.props.token )
@@ -25,78 +32,92 @@ class NavBar extends Component {
     };
 
     render(){
+        const { toggle } = this.state
         return(
-            <nav className="nav" id="navbar">
-                <div className="nav-content">
-                    <ul className="nav-items">
-                        <li className="nav-item">
-                            <Link
-                                activeClass="active"
-                                to="welcome"
-                                spy={true}
-                                smooth={true}
-                                offset={-60}
-                                duration= {700}
-                            >
-                                HOME
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                activeClass="active"
-                                to="about"
-                                spy={true}
-                                smooth={true}
-                                offset={-60}
-                                duration= {700}
-                            >
-                                WHAT WE DO
-                            </Link>
-                        </li>
-                        
-                        <li className="nav-item">
-                            <Link
-                                activeClass="active"
-                                to="work"
-                                spy={true}
-                                smooth={true}
-                                offset={-60}
-                                duration= {700}
-                            >
-                                WORK
-                            </Link> 
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                activeClass="active"
-                                to="workflow"
-                                spy={true}
-                                smooth={true}
-                                offset={-60}
-                                duration= {700}
-                            >
-                                WORKFLOW
-                            </Link> 
-                        </li>
+            <>
+                <button onClick={this.onToggle}><img src={hamburger} alt="menu icon"></img></button>
+                <Spring 
+                from={{ height: toggle ? 0 : 'auto' }}
+                to={{ height: toggle ? 'auto' : 0 }}
+                >
+                    { props => (
+                        <animated.div style={props}>
+                            <nav className="nav" id="navbar">
+                                <div className="nav-content">
+                                    <ul className="nav-items">
+                                        <li className="nav-item">
+                                            <Link
+                                                activeClass="active"
+                                                to="welcome"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-60}
+                                                duration= {700}
+                                            >
+                                                HOME
+                                            </Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link
+                                                activeClass="active"
+                                                to="about"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-60}
+                                                duration= {700}
+                                            >
+                                                WHAT WE DO
+                                            </Link>
+                                        </li>
+                                        
+                                        <li className="nav-item">
+                                            <Link
+                                                activeClass="active"
+                                                to="work"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-60}
+                                                duration= {700}
+                                            >
+                                                WORK
+                                            </Link> 
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link
+                                                activeClass="active"
+                                                to="workflow"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-60}
+                                                duration= {700}
+                                            >
+                                                WORKFLOW
+                                            </Link> 
+                                        </li>
 
-                        <li className="nav-item">
-                            <Link
-                                activeClass="active"
-                                to="contact"
-                                spy={true}
-                                smooth={true}
-                                offset={65}
-                                duration= {700}
-                            >
-                                CONTACT
-                            </Link>
-                        </li>
+                                        <li className="nav-item">
+                                            <Link
+                                                activeClass="active"
+                                                to="contact"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={65}
+                                                duration= {700}
+                                            >
+                                                CONTACT
+                                            </Link>
+                                        </li>
 
-                        {this.renderUser()}
+                                        {this.renderUser()}
 
-                    </ul> 
-                </div>
-            </nav>
+                                    </ul> 
+                                </div>
+                            </nav>
+                        </animated.div>
+                    )}
+                </Spring>
+            </>
+
         )
     }
 };
